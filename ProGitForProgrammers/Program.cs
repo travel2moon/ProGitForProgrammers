bff16354ed2cfe35b282af82137d8c2545b9ee96
moon seo <travel2moon@gmail.com>
2022-03-22 16:22:25 -0500

    Add writeln indicating we are in commandline

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in Visual Studio");
+Console.WriteLine("I just added this in the commnd line repo");
